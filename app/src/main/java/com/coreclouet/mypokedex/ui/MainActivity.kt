d9ba89b1c5c6577d0f51159870df52b87ec9c625
Corentin CLOUET <clouetcorentin@gmail.com>
2021-08-26 11:52:50 +0200

    Fix UI issues

--- app/src/main/java/com/coreclouet/mypokedex/ui/MainActivity.kt
+++ app/src/main/java/com/coreclouet/mypokedex/ui/MainActivity.kt
@@ -28,6 +28,7 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
         val appBarConfiguration = AppBarConfiguration(navController.graph)
+
         binding.tbMain.setupWithNavController(
             navController,
             appBarConfiguration
